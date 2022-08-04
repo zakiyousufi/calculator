d79c1adf52d97c401d25a2ff0764b4e0af8bc3db
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-08-04 11:41:57 +0430

    Add calculator fanction

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,10 @@
 import React, { useState } from 'react';
 import './Calculator.css';
 
+function Calculator() {
+  const output = { total: '0', next: '', operation: '' };
+  const [obj, setObj] = useState(output);
+
+}
+
 export default Calculator;
