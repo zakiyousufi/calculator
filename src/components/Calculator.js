63ccc53b83a6837dc9c5cf23343d964f2e8dc275
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-08-04 11:43:11 +0430

    Add opration handler fanction

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,6 +5,10 @@ function Calculator() {
   const output = { total: '0', next: '', operation: '' };
   const [obj, setObj] = useState(output);
 
+  function operationsHandler(name) {
+    const result = calculate(obj, name);
+    setObj({ ...obj, ...result });
+  }
 }
 
 export default Calculator;
